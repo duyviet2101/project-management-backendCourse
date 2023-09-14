f303ad8edbaaf6543abb83f7a4f4791770dbb705
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-14 10:53:05 +0700

    truyền dữ liệu vào trang /admin/products, trạng thái active, deleted

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,7 +2,9 @@ const Product = require('../../models/product.model.js')
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find({
+        deleted: false
+    });
     
     res.render("./admin/pages/products/index.pug", {
         pageTitle: 'Danh sách sản phẩm',

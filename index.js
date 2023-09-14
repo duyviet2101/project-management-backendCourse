885a535784c537833d9cd9ba97e552b536656fb0
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-14 09:18:29 +0700

    6.3 Cấu hình db

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const express = require("express");
+const database = require('./config/database')
+
+database.connect()
 
 const route = require("./routes/client/index.route")
 

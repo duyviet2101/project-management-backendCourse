981b035bf10cc4b3bfd7a77264c042986438ff76
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-15 15:39:26 +0700

    tính năng lọc theo trạng thái: tất cả, active, inactive

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,13 +2,54 @@ const Product = require('../../models/product.model.js')
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    });
+
+    //! tạo mảng gồm thông tin 3 nút bấm
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status:"",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status:"active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ];
+
+    if (req.query.status) {
+        const index = filterStatus.findIndex((item) => {
+            return item.status == req.query.status;
+        })
+
+        filterStatus[index].class = "active"
+    } else {
+        const index = filterStatus.findIndex((item) => {
+            return item.status == "";
+        })
+
+        filterStatus[0].class = "active"
+    }
+
+
+    let find = {
+        deleted: false,
+    };
+
+    if (req.query.status) {
+        find.status = req.query.status
+    }
+    const products = await Product.find(find);
     
     res.render("./admin/pages/products/index.pug", {
         pageTitle: 'Danh sách sản phẩm',
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
 

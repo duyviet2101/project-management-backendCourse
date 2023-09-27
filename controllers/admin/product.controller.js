239fe11095cac69bb8057ab8114d4afc28314ea2
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-27 22:13:15 +0700

    tạo mới sản phẩm - upload ảnh

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -131,6 +131,10 @@ module.exports.createPost = async (req, res) => {
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
     req.body.position = req.body.position ? parseInt(req.body.position) : countProduct + 1;
+
+    if (req.file && req.file.filename) {
+        req.body.thumbnail = `/uploads/${req.file.filename}`
+    }
     
     const product = new Product(req.body);
     await product.save();
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router();
 
+//!config multer
+const multer = require('multer');
+const storageMulterHelper = require('../../helpers/storageMulter.js');
+const storage = storageMulterHelper();
+const upload = multer({storage: storage});
+//!end config multer
 const controller = require('../../controllers/admin/product.controller')
 
 router.get("/", controller.index)
@@ -13,6 +19,6 @@ router.delete("/delete/:id", controller.deleteItem)
 
 router.get("/create", controller.create)
 
-router.post("/create", controller.createPost)
+router.post("/create", upload.single('thumbnail'), controller.createPost)
 
 module.exports = router

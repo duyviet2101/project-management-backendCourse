5251f28eba3d591ff7ed20ffe9ed60a0019919eb
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-07 14:54:32 +0700

    chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -21,7 +21,7 @@ module.exports.create = async (req, res) => {
   });
 }
 
-// [GET] /admin/roles/createPost
+// [POST] /admin/roles/createPost
 module.exports.createPost = async (req, res) => {
   const record = new Role(req.body);
   await record.save();
@@ -29,4 +29,34 @@ module.exports.createPost = async (req, res) => {
   req.flash("success", "Thêm nhóm quyền thành công");
   
   res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await Role.findOne({
+      _id: id,
+      deleted: false
+    });
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      data: data
+    });
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+  }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  await Role.updateOne({ _id: id }, req.body);
+
+  req.flash("success", "Cập nhật nhóm quyền thành công");
+  
+  res.redirect("back");
 }

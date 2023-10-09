a654aa1a0b9427e33a68ce519e0f20e25bf5988f
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-09 14:48:39 +0700

    middleware check đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,17 +5,19 @@ const trashRoute = require('./trash.route.js')
 const roleRoutes = require("./role.route");
 const accountRoutes = require('./account.route.js')
 const authRoutes = require('./auth.route.js')
+const authMiddleware = require('../../middlewares/admin/auth.middleware.js')
 
 const systemConfig = require('../../config/system.js')
 
 module.exports = (app) => {
     const PATH_ADMIN = '/' + systemConfig.prefixAdmin;
 
-    app.use(PATH_ADMIN + '/dashboard', dashboardRoute)
-    app.use(PATH_ADMIN + '/products', productRoute)
-    app.use(PATH_ADMIN + '/products-category', productsCategoryRoute)
-    app.use(PATH_ADMIN + '/products/trash', trashRoute)
-    app.use(PATH_ADMIN + "/roles", roleRoutes);
-    app.use(PATH_ADMIN + "/accounts", accountRoutes);
+
+    app.use(PATH_ADMIN + '/dashboard',authMiddleware.requireAuth, dashboardRoute)
+    app.use(PATH_ADMIN + '/products',authMiddleware.requireAuth, productRoute)
+    app.use(PATH_ADMIN + '/products-category', authMiddleware.requireAuth, productsCategoryRoute)
+    app.use(PATH_ADMIN + '/products/trash', authMiddleware.requireAuth, trashRoute)
+    app.use(PATH_ADMIN + "/roles", authMiddleware.requireAuth, roleRoutes);
+    app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
     app.use(PATH_ADMIN + "/auth", authRoutes);
 }

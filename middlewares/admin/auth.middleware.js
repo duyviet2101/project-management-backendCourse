754131c61f4e853e068a5c31840c0febe0ad6210
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-11 22:14:33 +0700

    autho phân quyền

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const {prefixAdmin} = require('../../config/system.js')
 const Account = require('../../models/account.model.js')
+const Role = require('../../models/role.model.js')
 
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
@@ -15,5 +16,11 @@ module.exports.requireAuth = async (req, res, next) => {
     return
   }
 
+  const role = await Role.findOne({
+    _id: user.role_id,
+  }).select('title permissions')
+
+  res.locals.user = user
+  res.locals.role = role
   next()
 }

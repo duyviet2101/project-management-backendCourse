355fe8056b9c248443e5d5a1526025a3e52fa529
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-16 22:08:20 +0700

    hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -4,6 +4,7 @@ const productsHelper = require('../../helpers/product.js')
 
 // [GET] /
 module.exports.index = async (req, res) => {
+    //! danh sach noi bat
     const productsFeatured = await Product.find({
         featured: "1",
         deleted: false,
@@ -12,8 +13,17 @@ module.exports.index = async (req, res) => {
 
     const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured)
 
+    //! danh sach sp moi
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"}).limit(6)
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
+
     res.render("./client/pages/home/index.pug", {
         pageTitle: 'Trang chủ',
-        productsFeatured: newProductsFeatured
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }

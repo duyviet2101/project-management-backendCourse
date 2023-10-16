62a6498fed0e6ee415bb5cc5e72c09780af7d15d
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-16 22:34:34 +0700

    lấy sản phẩm theo danh mục sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,17 +1,20 @@
 const Product = require('../../models/product.model.js')
+const ProductCategory = require('../../models/product-category.model.js')
 
 const productsHelper = require('../../helpers/product.js')
 
 // [GET] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
-        deleted: false,
-        status: "active"
-    })
-    .sort({position: 'desc'});
+            deleted: false,
+            status: "active"
+        })
+        .sort({
+            position: 'desc'
+        });
 
     const newProducts = productsHelper.priceNewProducts(products)
-    
+
     res.render("./client/pages/products/index.pug", {
         pageTitle: 'Danh sách sản phẩm',
         products: newProducts
@@ -38,5 +41,52 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
         res.redirect("/")
     }
-    
+
+}
+
+
+// [GET] /products/:SlugCategory
+module.exports.category = async (req, res) => {
+    const category = await ProductCategory.findOne({
+        deleted: false,
+        status: "active",
+        slug: req.params.slugCategory
+    })
+
+    const getSubCategory = async (parentId) => {
+        const subs = await ProductCategory.find({
+            parent_id: parentId,
+            status: "active",
+            deleted: false,
+        });
+
+        let allSub = [...subs];
+
+        for (const sub of subs) {
+            const childs = await getSubCategory(sub.id);
+            allSub = allSub.concat(childs);
+        }
+
+        return allSub;
+    }
+
+    const listSubCategory = await getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+            deleted: false,
+            status: "active",
+            product_category_id: {$in: [category.id, ...listSubCategoryId]}
+        })
+        .sort({
+            position: 'desc'
+        });
+
+    const newProducts = productsHelper.priceNewProducts(products)
+
+    res.render("./client/pages/products/index.pug", {
+        pageTitle: `${category.title}`,
+        products: newProducts
+    });
 }

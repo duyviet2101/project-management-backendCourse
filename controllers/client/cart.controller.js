42457016dc16f60f79d8ab6294b6b0ebf826c1aa
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-18 21:30:14 +0700

    trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,5 +1,7 @@
 const Cart = require('../../models/cart.model.js')
+const Product = require('../../models/product.model.js')
 
+const productHelper = require('../../helpers/product.js')
 
 // [POST] /cart/add/:productId
 module.exports.addPost =async (req, res, next) => {
@@ -41,4 +43,37 @@ module.exports.addPost =async (req, res, next) => {
   }
 
   res.redirect("back")
+}
+
+
+//[GET] /cart
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId
+
+  const cart = await Cart.findById({
+    _id: cartId
+  }).lean()
+
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+
+      const productInfo = await Product.findOne({
+        _id: productId
+      }).lean()
+
+      productInfo.priceNew = productHelper.priceNewProduct(productInfo)
+
+      item.productInfo = productInfo
+
+      item.totalPrice = item.quantity * productInfo.priceNew
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0)
+
+  res.render('client/pages/cart/index', {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  })
 }

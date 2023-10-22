e4436f4ae57640ad47c1ead3ac39ef421822c68e
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-22 10:47:11 +0700

    xem bài viết theo danh mục client

--- controllers/client/post.controller.js
+++ controllers/client/post.controller.js
@@ -1,5 +1,6 @@
 const Post = require('../../models/post.model.js')
 const PostCategory = require('../../models/posts-category.model.js')
+const Product = require('../../models/product.model.js')
 
 // GET /posts
 module.exports.index = async (req, res) => {
@@ -14,7 +15,7 @@ module.exports.index = async (req, res) => {
     pageTitle: 'Danh sách bài viết',
     posts
   })
-  
+
 }
 
 // GET /posts/detail/:slug
@@ -23,7 +24,8 @@ module.exports.detail = async (req, res) => {
 
   const post = await Post.findOne({
     slug: slug,
-    deleted: false
+    deleted: false,
+    status: 'active'
   }).lean()
 
   post.category = await PostCategory.findOne({
@@ -33,5 +35,48 @@ module.exports.detail = async (req, res) => {
   res.render('client/pages/posts/detail', {
     pageTitle: post.title,
     post
+  })
+}
+
+// GET /posts/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await PostCategory.findOne({
+    deleted: false,
+    status: 'active',
+    slug: req.params.slugCategory
+  })
+
+  const getSubCategory = async (parentId) => {
+    const subs = await PostCategory.find({
+      parent_id: parentId,
+      status: "active",
+      deleted: false,
+    });
+
+    let allSub = [...subs];
+
+    for (const sub of subs) {
+      const childs = await getSubCategory(sub.id);
+      allSub = allSub.concat(childs);
+    }
+
+    return allSub;
+  }
+
+  const listSubCategory = await getSubCategory(category.id);
+
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+
+  const posts = await Post.find({
+    deleted: false,
+    status: 'active',
+    post_category_id: {$in: [category.id, ...listSubCategoryId]}
+  }).sort({
+    position: 'desc'
+  })
+
+  res.render('client/pages/posts/index', {
+    pageTitle: category.title,
+    posts
   })
 }

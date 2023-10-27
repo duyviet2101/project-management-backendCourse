6890fccd88ff1340a738a248d8262b8a22ec4943
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-27 10:47:45 +0700

    bắt đầu làm tính năng chat, làm giao diện chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -10,6 +10,7 @@ const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
 const postRoutes = require("./post.route");
+const chatRoutes = require("./chat.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category)
@@ -33,4 +34,6 @@ module.exports = (app) => {
   app.use("/user", userRoutes)
 
   app.use("/posts", postRoutes)
+
+  app.use("/chat", chatRoutes)
 }

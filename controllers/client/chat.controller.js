7dfe0a10c194d39dec4ba822fe8e3528ac458bb6
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-27 14:49:48 +0700

    chat realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,16 +1,47 @@
+const Chat = require('../../models/chat.model.js')
+const User = require('../../models/user.model.js')
+
 // GET /chat
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName
+
   // !socket.io
-  _io.on('connection', (socket) => {
-    console.log('a user connected:::', socket.id);
-    socket.on('disconnect', () => {
-      console.log('user disconnected:::', socket.id);
-    })
+  _io.once('connection', (socket) => {
+    socket.on('CLIENT_SEND_MESSAGE', async (content) => {
+      // luu vao database
+      const chat = await Chat.create({
+        user_id: userId,
+        content: content
+      })
+
+      // tra data ve client
+      _io.emit('SERVER_RETURN_MESSAGE', {
+        userId,
+        fullName,
+        content
+      })
+    });
   });
   // !end socket.io
 
+  // !lay data in ra giao dien
+  const chats = await Chat.find({
+    deleted: false
+  })
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id
+    }).select('fullName')
+
+    chat.infoUser = infoUser
+  }
+  // !end lay data in ra giao dien
+
+
 
   res.render('client/pages/chat/index', {
-    pageTitle: 'Chat'
+    pageTitle: 'Chat',
+    chats
   })
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,37 @@
-var socket = io();
+// !CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector('.chat .inner-form');
+if (formSendData) {
+  formSendData.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const content = e.target.elements.content.value;
+    if (content) {
+      socket.emit('CLIENT_SEND_MESSAGE', content);
+      e.target.elements.content.value = '';
+    }
+  })
+}
+// !end CLIENT_SEND_MESSAGE
+
+// !SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const body = document.querySelector('.chat .inner-body')
+  const myId = document.querySelector("[my-id]").getAttribute('my-id')
+
+  const div = document.createElement('div')
+  
+  let htmlFullName = "";
+
+  if (myId == data.userId) {
+    div.classList.add('inner-outgoing')
+  } else {
+    div.classList.add('inner-incoming')
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`
+  }
+  div.classList.add('inner-incoming')
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `
+  body.appendChild(div)
+})
+// ! end SERVER_RETURN_MESSAGE

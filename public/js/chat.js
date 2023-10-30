11e6f1d9140c2dc639a4acbc5450ba5ff18cf546
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-10-30 21:56:01 +0700

    thêm tính năng chèn icon cho chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 //! CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
@@ -46,3 +48,27 @@ if (bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 //! End Scroll Chat To Bottom
+
+//! emoji-picker
+//? show popup
+const buttonIcon = document.querySelector('.button-icon');
+if (buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  Popper.createPopper(buttonIcon, tooltip)
+
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle('shown');
+  }
+}
+
+//? insert icon to input
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode;
+    inputChat.value += icon;
+  });
+}
+//! end emoji-picker
